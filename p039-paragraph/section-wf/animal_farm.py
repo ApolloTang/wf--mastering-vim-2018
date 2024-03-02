c65916e2a7bbb6fb6a80bf85dd11129a64b66289
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2024-03-02 21:56:04 +0800

    update

--- p039-paragraph/section-wf/animal_farm.py
+++ p039-paragraph/section-wf/animal_farm.py
@@ -8,11 +8,15 @@ def add_animal(farm, animal):
     farm.add(animal)
     return farm
 
+
+
+
 def main(animals):
     farm = set()
     for animal in animals:
         farm = add_animal(farm, animal)
     print("We've got some animals on the farm:", ', '.join(farm) + '.')
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
